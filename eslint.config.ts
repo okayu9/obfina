db6cee53140bb817c56e45b21202c00f37d7e571
Yumeto Inaoka <hi@okayu.jp>
2026-06-01 01:01:25 +0900

    Use JavaScript Svelte config for CI

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.ts';
+import svelteConfig from './svelte.config.js';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
